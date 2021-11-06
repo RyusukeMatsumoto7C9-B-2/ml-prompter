70e19ffd120eb992667bccb702c05b3d7625aabb
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2021-11-06 23:02:38 +0900

    MagicLeapのロゴを移動する茶目っ気スクリプトを追加
    実装するかどうかは未定

---
+++ ml-prompter-windows/Assets/ml-prompter/Scripts/PC/MagicLeapIconMover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ml_prompter.Pc
+{
+    /// <summary>
+    /// ちょっとした茶目っ気、MagicLeapのアイコンを上下に動かしたい.
+    /// </summary>
+    public class MagicLeapIconMover : MonoBehaviour
+    {
+
+        private void Update()
+        {
+            
+        }
+    }
+}
+
+    
