7444727e6cff01b0018dfcf023d0b288bf4f98f2
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2021-11-06 22:47:22 +0900

    利用しなくなったテスト用コードを削除

--- ml-prompter-windows/Assets/ml-prompter/Scripts/PC/WindowsInput.cs
+++ ml-prompter-windows/Assets/ml-prompter/Scripts/PC/WindowsInput.cs
@@ -11,42 +11,11 @@ namespace ml_prompter.Pc
     /// </summary>
     public class WindowsInput : MonoBehaviour
     {
-        // TODO : テスト用.
-        private WindowsInputProxy ttt = new WindowsInputProxy();
-        
-        
-        // Start is called before the first frame update
-        void Start()
-        {
-            //StartCoroutine(A());
-        }
-
-
-        private IEnumerator A()
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(1);
-                ttt.MouseLeftButtonDown();
-            }
-        }
-        
-
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 Application.Quit();
-            }
-
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                ttt.MouseLeftButtonDown();
-            }
-            else if (Input.GetKeyDown(KeyCode.S))
-            {
-                ttt.MouseRightButtonDown();
             }
         }
     }
