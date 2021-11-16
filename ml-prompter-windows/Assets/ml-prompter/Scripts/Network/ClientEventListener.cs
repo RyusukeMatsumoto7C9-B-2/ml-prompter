e273b201d044e628a89a8da151d300ce69869487
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2021-11-17 02:37:09 +0900

    ClientEventListenerを追加

---
+++ ml-prompter-windows/Assets/ml-prompter/Scripts/Network/ClientEventListener.cs
@@ -0,0 +1,16 @@
+﻿using Photon.Bolt;
+
+
+namespace ml_prompter.Network
+{
+    /// <summary>
+    /// MagicLeap側のイベントリスナ.
+    /// </summary>
+    public class ClientEventListener : GlobalEventListener
+    {
+        public override void OnEvent(ScreenCaptureEvent ev)
+        {
+            
+        }
+    }
+}
