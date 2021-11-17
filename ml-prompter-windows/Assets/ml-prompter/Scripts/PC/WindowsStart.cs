dd5088635a73ae560c3ceabd83a69b419919bc09
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2021-11-17 21:57:22 +0900

    起動時に適当なウィンドウサイズにした

--- ml-prompter-windows/Assets/ml-prompter/Scripts/PC/WindowsStart.cs
+++ ml-prompter-windows/Assets/ml-prompter/Scripts/PC/WindowsStart.cs
@@ -15,7 +15,7 @@ namespace ml_prompter.Pc
 
         private void Start()
         {
-            Screen.SetResolution(1920, 1080, true);
+            Screen.SetResolution(1024, 760, false);
             BoltLauncher.StartServer();
         }
 
