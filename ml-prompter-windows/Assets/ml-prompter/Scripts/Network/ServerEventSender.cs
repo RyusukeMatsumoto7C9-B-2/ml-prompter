7ae69b21afc68910ae4a5435df3df25db2736a9d
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2021-11-23 20:53:31 +0900

    不要になったコードを削除

--- ml-prompter-windows/Assets/ml-prompter/Scripts/Network/ServerEventSender.cs
+++ ml-prompter-windows/Assets/ml-prompter/Scripts/Network/ServerEventSender.cs
@@ -35,47 +35,13 @@ namespace ml_prompter.Network
             IsSendingScreenShotData = true;
 
             Debug.Log($"ML_ CaptureStringsCount {captureStrings.Length} {captureStrings[captureStrings.Length - 1]}");
-            #if false
-            int count = 0;
-            while (count < captureStrings.Length)
-            {
-                var ev = ScreenCaptureEvent.Create();
-
-                Debug.Log($"while Count {count}");
-                ev.CaptureString1 = count < captureStrings.Length ? captureStrings[count] : "-e";
-                count++;
-
-                /*
-                Debug.Log($"while Count {count}");
-                ev.CaptureString2 = count < captureStrings.Length ? captureStrings[count] : "-e";
-                count++;
-                
-                Debug.Log($"while Count {count}");
-                ev.CaptureString3 = count < captureStrings.Length ? captureStrings[count] : "-e";
-                count++;
-                */
-                
-                /*
-                Debug.Log($"while Count {count}");
-                ev.CaptureString4 = count < captureStrings.Length ? captureStrings[count] : "-e";
-                count++;
-
-                Debug.Log($"while Count {count}");
-                ev.CaptureString5 = count < captureStrings.Length ? captureStrings[count] : "-e";
-                count++; // 次の為にインクリメント.
-                */
-                
-                ev.Send();
-                yield return new WaitForEndOfFrame();
-            }
-            #endif
 
             foreach (var data in captureStrings)
             {
                 var ev = ScreenCaptureEvent.Create();
                 ev.CaptureString1 = data;
                 ev.Send();
-                yield return new WaitForSeconds(0.03f);
+                yield return new WaitForEndOfFrame();
             }
             Debug.Log("ML_ 送信完了.");
             IsSendingScreenShotData = false;
