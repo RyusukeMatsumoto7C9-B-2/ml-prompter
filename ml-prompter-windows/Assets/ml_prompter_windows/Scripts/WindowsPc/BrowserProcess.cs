067bfc33e9f5ddf3858588a8d2754ce62c1ce6a5
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2022-03-14 23:27:49 +0900

    外部プロセスを起動するクラスを追加

---
+++ ml-prompter-windows/Assets/ml_prompter_windows/Scripts/WindowsPc/BrowserProcess.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+namespace ml_prompter.WindowsPc
+{
+    public class BrowserProcess
+    {
+        private string Path => Application.streamingAssetsPath + "/PCSetup/Browser/index.html";
+        private Process process = new Process();
+
+
+        public void Start()
+        {
+            process.StartInfo.FileName = Path;
+            process.Start();
+        }
+
+
+        public void Close()
+        {
+            if (!process.HasExited)
+            {
+                process.CloseMainWindow();
+            }
+            process.Close();
+            process.Dispose();
+        }
+
+
+    }
+}
---
+++ ml-prompter-windows/Assets/ml_prompter_windows/Scripts/WindowsPc/ServerProcess.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+namespace ml_prompter.WindowsPc
+{
+    public class ServerProcess
+    {
+        private string Path => Application.streamingAssetsPath + "/PCSetup/Server/server.py";
+
+        private Process process = new Process();
+            
+            
+
+        public void Start()
+        {
+            process.StartInfo.FileName = Path;
+            process.Start();
+        }
+
+
+
+        public void Close()
+        {
+            if (!process.HasExited)
+            {
+                process.CloseMainWindow();
+            }
+            process.Close();
+            process.Dispose();
+        }
+
+
+    }
+}
