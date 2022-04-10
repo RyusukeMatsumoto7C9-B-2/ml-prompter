e3fa2d627b643ea2e5d2d4f8275cab35e96d0c61
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2022-04-10 20:27:05 +0900

    WebSocketでサーバーからのレスポンスを取得できるようにした

--- ml-prompter-windows/Assets/StreamingAssets/PCSetup/Browser/main.js
+++ ml-prompter-windows/Assets/StreamingAssets/PCSetup/Browser/main.js
@@ -82,20 +82,36 @@ function sleep(msec) {
   })
 }
 
-function serverStart() {
-  try {
-    document.dispatchEvent(new KeyboardEvent("keyup", {key: "ArrowLeft"}));
-    document.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowLeft"}));
-    document.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowLeft"}));
-    document.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowLeft"}));
-    alert("キー入力を代行したよ");
-  }
-  catch(e)
-  {
-    alert(e);
-  }
-}
 
+// WebSocketでサーバーと接続.
+function serverStart() {
+  console.log(location.pathname);
+
+  console.log("WebSocketでサーバーに接続.");
+  var connection = new WebSocket('ws://localhost:8000/ws/');
+  connection.onopen = function(event) {
+    console.log("通信接続イベント受信");
+    console.log(event.data);
+  };
+
+  //エラー発生
+  connection.onerror = function(error) {
+    console.log("エラー発生イベント受信");
+    console.log(error.data);
+  };
+
+  //メッセージ受信
+  connection.onmessage = function(event) {
+    console.log("メッセージ受信");
+    console.log(event.data);
+  };
+
+  //切断
+  connection.onclose = function() {
+    console.log("通信切断イベント受信");
+  };
+}
+ 
 
 async function start() {
   try {
