5d91493af3f26def9e7d76772750da577a487e0a
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2022-05-21 18:44:53 +0900

    ダミーデータを使ってのテスト

--- ml-prompter-magicleap/Assets/ml_prompter/Scripts/SpeakerNote/SpeakerNoteUi.cs
+++ ml-prompter-magicleap/Assets/ml_prompter/Scripts/SpeakerNote/SpeakerNoteUi.cs
@@ -29,39 +29,30 @@ namespace ml_promter.SpeakerNote
 
         [SerializeField]
         private Image noteBackground;
+        
+        private SpeakerNote speakerNote;
 
 
         private IEnumerator Start()
         {
+            speakerNote = new SpeakerNote();
+
+            // テストとしてダミーのページを登録.
+            {
+                speakerNote.AddPage(new SpeakerNotePage("1ページ目\nほげほげ"));
+                speakerNote.AddPage(new SpeakerNotePage("2ページ目\nふがふが"));
+                speakerNote.AddPage(new SpeakerNotePage("3ページ目\nじぇじぇじぇ"));
+                speakerText.text = speakerNote.CurrentPage().Value;
+            }
+
             Debug.Log("SpeakerNoteUI Start()");
             yield return new WaitUntil(() => MLInput.IsStarted);
             Debug.Log("MLInputが起動しました.");
-            
-            nextPageButton.onClick.AddListener(() =>
-            {
-                webRtc.SendTextMessage("NextButton");
-                Debug.Log("nextPageButton押下したよ");
-                
-            });        
-            nextSlideButton.onClick.AddListener(() =>
-            {
-                webRtc.SendTextMessage("NextSlide");
-                Debug.Log("nextSlideButton押下したよ"); 
-                
-            });        
 
-            prevPageButton.onClick.AddListener(() =>
-            {
-                webRtc.SendTextMessage("PrevPage");
-                Debug.Log("prevPageButton押下したよ"); 
-                
-            });        
-            prevSlideButton.onClick.AddListener(() =>
-            {
-                webRtc.SendTextMessage("PrevSlide");
-                Debug.Log("prevSlide押下したよ"); 
-                
-            });
+            nextPageButton.onClick.AddListener(OnNextPage);
+            nextSlideButton.onClick.AddListener(OnNextSlide);
+            prevPageButton.onClick.AddListener(OnPreviousPage);
+            prevSlideButton.onClick.AddListener(OnPreviousSlide);
             
             MLInput.OnControllerTouchpadGestureStart += OnTouchpadGestureStart;
         }
@@ -85,10 +76,49 @@ namespace ml_promter.SpeakerNote
             noteBackground.color = new Color(0f, 0f, 0f, value);
         }
 
-        
+
+        private void OnNextPage()
+        {
+            speakerNote.Next();
+            speakerText.text = speakerNote.CurrentPage().Value;
+            Debug.Log("nextPageButton押下したよ");
+        }
+
+
+        private void OnNextSlide()
+        {
+            speakerNote.Next();
+            speakerText.text = speakerNote.CurrentPage().Value;
+            
+            // スライド遷移のメッセージ送信.
+            webRtc.SendTextMessage("NextSlide");
+            Debug.Log("nextSlideButton押下したよ"); 
+        }
+
+
+        private void OnPreviousPage()
+        {
+            speakerNote.Previous();
+            speakerText.text = speakerNote.CurrentPage().Value;
+            Debug.Log("prevPageButton押下したよ"); 
+        }
+
+
+        private void OnPreviousSlide()
+        {
+            speakerNote.Previous();
+            speakerText.text = speakerNote.CurrentPage().Value;
+            
+            // スライド遷移のメッセージ送信.
+            webRtc.SendTextMessage("PrevSlide");
+            Debug.Log("prevSlide押下したよ"); 
+                
+        }
+
+
         public void AddPage(string text)
         {
-            
+            speakerNote.AddPage(new SpeakerNotePage(text));    
         }
     }
 }
