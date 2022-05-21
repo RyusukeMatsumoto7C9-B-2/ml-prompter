817856eb92f44da05db7bbd3e0edc714e2bb98cf
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2022-05-21 14:14:17 +0900

    EditTestができるようにした

---
+++ ml-prompter-magicleap/Assets/ml_prompter/Test/EditTests/SpeakerNotePageTests.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+using ml_promter.SpeakerNote;
+
+
+public class SpeakerNotePageTests
+{
+    
+    [Test]
+    public void SpeakerNotePageIsInvalidTest()
+    {
+        var page = new SpeakerNotePage("");
+        Assert.IsFalse(page.IsValidPage);
+    }
+
+
+    [Test]
+    public void SpeakerNotePageInvalidTest()
+    {
+        var page = new SpeakerNotePage("hoge");
+        Assert.IsTrue(page.IsValidPage);
+    }
+
+
+    /// <summary>
+    /// 初期化時に設定した文字列と同じ文字列が格納されるのを確認.
+    /// </summary>
+    [Test]
+    public void SpeakerNotePageTextEqualInitialTexttTest()
+    {
+        const string INITIAL_TEXT = "Hoge";
+        var page = new SpeakerNotePage(INITIAL_TEXT);
+        Assert.IsTrue(INITIAL_TEXT.Equals(page.Value));
+    }
+
+}
